583cab0638687d1c618ccf1a3fc1beda0dc50081
bXiilzy <jiranankredkla12@gmail.com>
2025-07-23 15:43:03 +0700

    upload

---
+++ webpage/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    """
+    Render the home page.
+    """
+    return render(request, 'home.html')
+
+
+def about(request):    
+    """
+    Render the about page.
+    """
+    return render(request, 'about.html')
+
+def contact(request):
+    """
+    Render the contact page.
+    """
+    return render(request, 'contact.html')
