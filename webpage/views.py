1e9aae7462b39120a8e8a476c8c3bfa498024961
bXiilzy <jiranankredkla12@gmail.com>
2025-08-21 23:39:51 +0700

    Update styles and fix template references for improved UI consistency

--- webpage/views.py
+++ webpage/views.py
@@ -2,27 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    """
-    Render the home page.
-    """
-    return render(request, 'home.html')
+def base(request):
+    return render(request, 'base.html')
 
-
-def about(request):    
-    """
-    Render the about page.
-    """
+def about(request):
     return render(request, 'about.html')
 
 def contact(request):
-    """
-    Render the contact page.
-    """
     return render(request, 'contact.html')
 
-def for_page(request):
-    """
-    Render the 'for' page.
-    """
-    return render(request, 'for.html')
+def home(request):
+    return render(request, 'home.html')
+
+def navbar(request):
+    return render(request, 'navbar.html')
+
+def for_view(request):
+    context = {}
+    context['message'] = "การวนซ้ำใน_Django"
+    
+    if request.method == 'POST' and request.POST.get('count') != '':
+        number = int(request.POST.get('count'))
+        context['count'] = list(range(1,number +1))
+    else:
+        context['count'] = list(range(1, 2))
+        
+    return render(request, 'for.html',context)
+
+def multipli(request):
+    context = {}
+    context['message'] = "ตารางสูตรคูณ"
+    
+    if request.method == 'POST' and request.POST.get('number') != '':
+        number = int(request.POST.get('number'))
+        context['number'] = number
+        context['table'] = [(i, number * i) for i in range(1, 13)]
+    else:
+        context['table'] = []
+        context['number'] = None
+        
+    return render(request, 'table.html', context)
